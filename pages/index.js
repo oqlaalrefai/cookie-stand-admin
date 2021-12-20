b10300cf1ca88b11b919819304d9a944b4d79d13
Oqla Alrefai <2015978054@ses.yu.edu.jo>
2021-12-21 01:10:48 +0200

    cookies admin

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,94 @@
 import Head from 'next/head'
+import React from "react";
 
 export default function Home() {
+  
+  const [data, setData] = React.useState()
+  
+  function handleClick(event){
+    event.preventDefault()
+    
+    let data={'location':event.target.location.value,'minCustomers':event.target.minimum.value,'maxCustomers':event.target.maximum.value, 'avgCookies':event.target.average.value}
+    setData(data)
+    
+  }
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main className="min-h-screen bg-green-50 ">
+        <Header />
+        <Form  onSubmit={handleClick}/>
+        <Display data={data}/>
+        <Footer/>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
     </div>
   )
 }
+
+
+function Header() {
+  return (
+    <header className='flex w-full p-4 font-serif text-4xl font-bold bg-emerald-400'>
+    <h1 className='text-5xl'>Cookie Stand Admin </h1>
+    </header>
+  )
+}
+
+function Form(props) {
+   
+  return (
+    <div> 
+    <form onSubmit={props.onSubmit} className="flex-col w-5/6 p-2 mx-auto my-8 rounded-md bg-emerald-300">
+      <fieldset>
+        <h1 className="my-3 text-2xl text-center" type="text">
+          Cookie Stand Admin
+        </h1>
+        <div className="flex w-100">
+          <label className="ml-16 ">Location</label>
+          <input name="location" className="flex w-10/12 ml-5 gray-100" />
+        </div>
+        <div className="flex w-4/5 gap-10 p-4 pb-5 ml-16 text-xs">
+          <div className="flex-col w-1/4 ">
+            <label>Minimum Customer per Hour</label>
+            <input type="integer" className="" name="minimum" />
+          </div>
+          <div className="flex-col w-1/4 ">
+            <label>Maximum Custumer per Hour</label>
+            <input type="integer" className="" name="maximum" />
+          </div>
+          <div className="flex-col w-1/4">
+            <div className="flex"></div>
+            <label>Average Cookies per Sales</label>
+            <input type="float" className="" name="average" />
+          </div>
+          <button type="submit" className='mt-6 text-xl h-11 bg-emerald-500 w-60'>
+            Create
+          </button>
+        </div>
+      </fieldset>
+    </form>
+    <p className='text-center'>Report Table Comming Soon ...
+      
+    </p><br/>
+    </div>
+  )
+}
+
+function Display({data}) {
+   
+  return (
+    <div>
+      <p className="my-3 text-center text-gray-600 text-1xl">{JSON.stringify(data)}<br/></p>
+    </div>    
+  )
+}
+
+function Footer() {
+  return (
+    <footer className="flex items-center w-full p-4 font-bold bg-emerald-400">&copy;2021</footer>
+  )
+}
+
